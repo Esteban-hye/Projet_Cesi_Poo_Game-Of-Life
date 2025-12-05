a45e3d50fd53c5f50ab50376abc903abc3fe0110
Jérémy.J <84200883+JeremyJrz@users.noreply.github.com>
2025-12-05 15:11:58 +0100

    Après avoir test l'interface

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -21,10 +21,10 @@ void Interface::ShowGrid(Grille* grille) {
     window.clear();
     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
 
-    for (int x = 0; x < grille->getLargeur(); ++x) {
-        for (int y = 0; y < grille->getHauteur(); ++y) {
+    for (int x = 0; x < grille->GetLargeur(); ++x) {
+        for (int y = 0; y < grille->GetLongeur(); ++y) {
 
-            if (grille->getCellule(x, y)->EstVivant()) {
+            if (grille->GetCellule(x, y)->EstVivant()) {
                 cell.setPosition(x * cellSize, y * cellSize);
                 window.draw(cell);
             }
